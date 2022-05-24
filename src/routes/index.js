829ddd1d2ba4b0ad5cc365ff53154c66518e489b
AbdoulNuru <abdoulniyigena@gmail.com>
2022-05-24 09:58:05 +0200

    ft: add church admin

--- src/routes/index.js
+++ src/routes/index.js
@@ -7,6 +7,7 @@ import Register from "../pages/register/register";
 import Home from "../components/home";
 import AddChurch from "../pages/add church/newChurch";
 import AddChurchLeader from "../pages/add leader/newChurchLeader";
+import AddChurchAdmin from "../pages/add admin/newChurchAdmin";
 
 axios.defaults.baseURL = "http://localhost:3600";
 
@@ -27,6 +28,11 @@ export default class index extends Component {
                   path="/add-church-leader"
                   exact
                   component={AddChurchLeader}
+                />
+                <Route
+                  path="/add-church-admin"
+                  exact
+                  component={AddChurchAdmin}
                 />
               </div>
             )}
